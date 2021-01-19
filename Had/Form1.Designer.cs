f4063a31c6e1424b78091af23ffca756fcfd7e8d
Grzeho1 <71527706+Grzeho1@users.noreply.github.com>
2021-01-19 21:12:01 +0100

    Add files via upload

---
+++ Had/Form1.Designer.cs
@@ -0,0 +1,152 @@
+﻿namespace Had
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Vyžaduje se proměnná návrháře.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Uvolněte všechny používané prostředky.
+        /// </summary>
+        /// <param name="disposing">hodnota true, když by se měl spravovaný prostředek odstranit; jinak false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Kód generovaný Návrhářem Windows Form
+
+        /// <summary>
+        /// Metoda vyžadovaná pro podporu Návrháře - neupravovat
+        /// obsah této metody v editoru kódu.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.Rychlost = new System.Windows.Forms.TrackBar();
+            this.lblRychlost = new System.Windows.Forms.Label();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.lblSkore = new System.Windows.Forms.Label();
+            this.Skore = new System.Windows.Forms.Label();
+            this.tlacitkoStart = new System.Windows.Forms.Button();
+            this.tlacitkoStop = new System.Windows.Forms.Button();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            ((System.ComponentModel.ISupportInitialize)(this.Rychlost)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // Rychlost
+            // 
+            this.Rychlost.Location = new System.Drawing.Point(12, 69);
+            this.Rychlost.Maximum = 100;
+            this.Rychlost.Name = "Rychlost";
+            this.Rychlost.Orientation = System.Windows.Forms.Orientation.Vertical;
+            this.Rychlost.Size = new System.Drawing.Size(45, 377);
+            this.Rychlost.TabIndex = 0;
+            this.Rychlost.Value = 50;
+            this.Rychlost.Scroll += new System.EventHandler(this.Rychlost_Scroll);
+            // 
+            // lblRychlost
+            // 
+            this.lblRychlost.AutoSize = true;
+            this.lblRychlost.Font = new System.Drawing.Font("Segoe Print", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.lblRychlost.Location = new System.Drawing.Point(6, 9);
+            this.lblRychlost.Name = "lblRychlost";
+            this.lblRychlost.Size = new System.Drawing.Size(95, 33);
+            this.lblRychlost.TabIndex = 1;
+            this.lblRychlost.Text = "Rychlost";
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.Black;
+            this.panel1.Location = new System.Drawing.Point(60, 45);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(570, 450);
+            this.panel1.TabIndex = 2;
+            // 
+            // lblSkore
+            // 
+            this.lblSkore.AutoSize = true;
+            this.lblSkore.Font = new System.Drawing.Font("Segoe Print", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.lblSkore.Location = new System.Drawing.Point(648, 184);
+            this.lblSkore.Name = "lblSkore";
+            this.lblSkore.Size = new System.Drawing.Size(75, 33);
+            this.lblSkore.TabIndex = 3;
+            this.lblSkore.Text = "Skore:";
+            // 
+            // Skore
+            // 
+            this.Skore.AutoSize = true;
+            this.Skore.Font = new System.Drawing.Font("Segoe Print", 36F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.Skore.Location = new System.Drawing.Point(653, 230);
+            this.Skore.Name = "Skore";
+            this.Skore.Size = new System.Drawing.Size(70, 84);
+            this.Skore.TabIndex = 4;
+            this.Skore.Text = "0";
+            // 
+            // tlacitkoStart
+            // 
+            this.tlacitkoStart.Font = new System.Drawing.Font("Segoe Print", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.tlacitkoStart.Location = new System.Drawing.Point(636, 27);
+            this.tlacitkoStart.Name = "tlacitkoStart";
+            this.tlacitkoStart.Size = new System.Drawing.Size(152, 52);
+            this.tlacitkoStart.TabIndex = 5;
+            this.tlacitkoStart.Text = "Start";
+            this.tlacitkoStart.UseVisualStyleBackColor = true;
+            this.tlacitkoStart.Click += new System.EventHandler(this.tlacitkoStart_Click);
+            // 
+            // tlacitkoStop
+            // 
+            this.tlacitkoStop.Font = new System.Drawing.Font("Segoe Print", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.tlacitkoStop.Location = new System.Drawing.Point(636, 85);
+            this.tlacitkoStop.Name = "tlacitkoStop";
+            this.tlacitkoStop.Size = new System.Drawing.Size(152, 52);
+            this.tlacitkoStop.TabIndex = 6;
+            this.tlacitkoStop.Text = "Stop";
+            this.tlacitkoStop.UseVisualStyleBackColor = true;
+            this.tlacitkoStop.Click += new System.EventHandler(this.tlacitkoStop_Click);
+            // 
+            // timer1
+            // 
+            this.timer1.Interval = 250;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Olive;
+            this.ClientSize = new System.Drawing.Size(800, 530);
+            this.Controls.Add(this.tlacitkoStop);
+            this.Controls.Add(this.tlacitkoStart);
+            this.Controls.Add(this.Skore);
+            this.Controls.Add(this.lblSkore);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.lblRychlost);
+            this.Controls.Add(this.Rychlost);
+            this.Name = "Form1";
+            this.Text = "Had";
+            ((System.ComponentModel.ISupportInitialize)(this.Rychlost)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TrackBar Rychlost;
+        private System.Windows.Forms.Label lblRychlost;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Label lblSkore;
+        private System.Windows.Forms.Label Skore;
+        private System.Windows.Forms.Button tlacitkoStart;
+        private System.Windows.Forms.Button tlacitkoStop;
+        private System.Windows.Forms.Timer timer1;
+    }
+}
+
---
+++ Had/Form1.cs
@@ -0,0 +1,293 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Drawing.Text;
+using System.Linq;
+using System.Media;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Had
+{
+    public partial class Form1 : Form
+    {
+
+        // ZAKLADNÍ PROMENNE
+
+        PictureBox[] hadCasti;
+        int hadVelikost;
+        Point lokace = new Point(120, 120);
+        string smer = "Prava";
+        bool zmenaSmeru = false;
+        
+        //Jídllo
+        PictureBox jidlo = new PictureBox();
+        Point lokaceJidla = new Point(0, 0);
+        public Form1()
+        {
+            InitializeComponent();
+            tlacitkoStop.Enabled = false;
+        }
+        
+
+        private void tlacitkoStart_Click(object sender, EventArgs e)
+        {
+            //znovu zapnutí hry 
+            panel1.Controls.Clear();
+            hadCasti = null;
+            Skore.Text = "0";
+            hadVelikost = 5;
+            smer = "Prava";
+            lokace = new Point(120, 120);
+            
+            //Start hry
+            vykresliHada();
+            vykresliJidlo();
+            timer1.Start();
+
+            //zakazani tlacitek start a rychlost
+            Rychlost.Enabled = false;
+            tlacitkoStart.Enabled = false;
+
+            //povoleni tlacitka stop
+
+            tlacitkoStop.Enabled = true;
+        }
+        private void vykresliHada()
+        {
+            hadCasti = new PictureBox[hadVelikost];
+
+            //vykresleni casti hada
+            for (int i = 0; i < hadVelikost; i++)
+            {
+                hadCasti[i] = new PictureBox();
+                hadCasti[i].Size = new Size(15, 15);
+                hadCasti[i].BackColor = Color.Red;
+                hadCasti[i].BorderStyle = BorderStyle.None;
+                hadCasti[i].Location = new Point(lokace.X - (15 * i), lokace.Y);
+                panel1.Controls.Add(hadCasti[i]);
+            }
+
+        }
+         
+        private void vykresliJidlo()
+        {
+            Random random = new Random();
+            int Xrandom = random.Next(38) * 15;
+            int Yrandom = random.Next(30) * 15;
+
+            bool naHadovi = true;
+
+            //Znovu vykreslení jidla v pripade že se objeví na pozici hada
+            while (naHadovi)
+            {
+
+                for (int i = 0; i < hadVelikost; i++)
+                {
+                    if (hadCasti[i].Location == new Point(Xrandom, Yrandom))
+                    {
+                        Xrandom = random.Next(38) * 15;
+                        Yrandom = random.Next(30) * 15;
+                    }
+                    else
+                    {
+                        naHadovi = false;
+                    }
+                }
+            }
+            // vykresleni jidla
+            if (naHadovi == false)
+            {
+                lokaceJidla = new Point(Xrandom, Yrandom);
+                jidlo.Size = new Size(15, 15);
+                jidlo.BackColor = Color.Yellow;
+                
+                jidlo.Location = lokaceJidla;
+                panel1.Controls.Add(jidlo);
+
+            }
+
+        }
+
+        private void Rychlost_Scroll(object sender, EventArgs e)
+        {
+            //zmena  rychlosti
+            timer1.Interval = 501 - (5 * Rychlost.Value);
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            pohyb();
+        }
+
+        private void pohyb()
+        {
+
+            Point point = new Point(0, 0);
+
+            // zvuk pro udalosti
+            SoundPlayer hadNaraz = new SoundPlayer(Properties.Resources.crash);
+            SoundPlayer hadEat = new SoundPlayer(Properties.Resources.eat);
+
+            //cyklus pro pohyb vsech casti pri zmene smeru
+            for (int i = 0; i < hadVelikost; i++)
+            {
+                if (i == 0)
+                {
+
+                    point = hadCasti[i].Location;
+                    if (smer == "Leva")
+                    {
+                        hadCasti[i].Location = new Point(hadCasti[i].Location.X - 15, hadCasti[i].Location.Y);
+                    }
+                    if (smer == "Prava")
+                    {
+                        hadCasti[i].Location = new Point(hadCasti[i].Location.X + 15, hadCasti[i].Location.Y);
+                    }
+                    if (smer == "Nahoru")
+                    {
+                        hadCasti[i].Location = new Point(hadCasti[i].Location.X, hadCasti[i].Location.Y - 15);
+                    }
+                    if (smer == "Dolu")
+                    {
+                        hadCasti[i].Location = new Point(hadCasti[i].Location.X, hadCasti[i].Location.Y + 15);
+                    }
+
+                }
+                else
+                {
+                    Point novyPoint = hadCasti[i].Location;
+                    hadCasti[i].Location = point;
+                    point = novyPoint;
+                }
+            }
+            // kolize s jidlem
+            
+            if (hadCasti[0].Location == lokaceJidla)
+            {
+                snezJidlo();
+                vykresliJidlo();
+                hadEat.Play();
+
+            }
+            //kolize se stenou
+            if (hadCasti[0].Location.X < 0 || hadCasti[0].Location.X >= 570 || hadCasti[0].Location.Y < 0 || hadCasti[0].Location.Y >= 450) 
+            {
+                zastavHru();
+                hadNaraz.Play();
+                
+            }
+            // kolize hada se sebou
+            
+            
+            for (int i = 3; i < hadVelikost; i++)
+            {
+                if (hadCasti[0].Location == hadCasti[i].Location)
+                {
+                    zastavHru();
+                    hadNaraz.Play();
+                }
+                
+            }
+            
+
+            zmenaSmeru = false;
+        }
+        //ovladani hada
+        protected override bool ProcessCmdKey(ref Message msg, Keys keyData)
+        {
+            if (keyData == (Keys.Up) && smer != "Dole" && zmenaSmeru != true)
+            {
+                smer = "Nahoru";
+                zmenaSmeru = true;
+                
+            }
+            if (keyData == (Keys.Down) && smer != "Nahoru" && zmenaSmeru != true)
+            {
+                smer = "Dolu";
+                zmenaSmeru = true;
+                
+            }
+            if (keyData == (Keys.Right) && smer != "Leva" && zmenaSmeru != true)
+            {
+                smer = "Prava";
+                zmenaSmeru = true;
+                
+            }
+            if (keyData == (Keys.Left) && smer != "Prava" && zmenaSmeru != true)
+            {
+                smer = "Leva";
+                zmenaSmeru = true;
+                
+            }
+            
+            
+
+
+            return base.ProcessCmdKey(ref msg, keyData);
+        }
+        private void snezJidlo()
+        {
+            hadVelikost++;
+            //zvetseni hada o 1 policko ,,, ulozeni stareho hada a vytvoreni vetsiho hada
+            PictureBox[] staryHad = hadCasti;
+            panel1.Controls.Clear();
+            hadCasti = new PictureBox[hadVelikost];
+            for (int i=0;i<hadVelikost;i++)
+            {
+                hadCasti[i] = new PictureBox();
+                hadCasti[i].Size = new Size(15, 15);
+                hadCasti[i].BackColor = Color.Red;
+                
+                if (i==0)
+                {
+                    hadCasti[i].Location = lokaceJidla;
+                }
+                else
+                {
+                    hadCasti[i].Location = staryHad[i - 1].Location;
+                }
+                panel1.Controls.Add(hadCasti[i]);
+            }
+            //skore
+            int dosavadniSkore = Int32.Parse(Skore.Text);
+            int noveSkore = dosavadniSkore + 10 ;
+            Skore.Text = noveSkore + "";
+        }
+
+        private void zastavHru()
+        {
+            timer1.Stop();
+            tlacitkoStart.Enabled = true;
+            Rychlost.Enabled = true;
+            tlacitkoStop.Enabled = false;
+            panel1.Controls.Clear();
+
+            //Konec hry
+            Label konec = new Label();
+            konec.Text = "KONEC HRY";
+            konec.ForeColor = Color.Red;
+            konec.Font = new Font("Sagoe Print", 50, FontStyle.Bold);
+            konec.Size = konec.PreferredSize;
+            konec.TextAlign = ContentAlignment.MiddleCenter;
+            konec.BringToFront();
+
+            // misto kde se ma objevit label konec hry
+            int X = panel1.Width / 2 - konec.Width / 2;
+            int Y = panel1.Height / 2 - konec.Height / 2;
+            konec.Location = new Point(X, Y);
+            panel1.Controls.Add(konec);
+            
+         }
+
+        private void tlacitkoStop_Click(object sender, EventArgs e)
+        {
+            
+            zastavHru();
+        }
+    }
+    
+}
